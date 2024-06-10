dc6dd0138734f9e8d4186dad3bfe9ec43f8d6299
Dipak Kumar Dubey <161556150+6388505232@users.noreply.github.com>
2024-06-09 17:42:16 -0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer" , "Web Designer" , "Full-Stack Developer" ,
+     "UX/UI Desiner" ],
+     typeSpeed: 50,
+     backSpeed: 100,
+     backDelay: 1000,
+     loop: true
+})
